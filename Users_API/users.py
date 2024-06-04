010b012767e1046862c621587441e36baad46423
JoseFrancisc0 <83974552+JoseFrancisc0@users.noreply.github.com>
2024-06-04 15:30:36 -0500

    Update users.py

--- Users_API/users.py
+++ Users_API/users.py
@@ -14,8 +14,8 @@ CORS(users_api)
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True, nullable = False)
-    email = db.Column(db.Column(db.String(100), nullable = False))
-    password = db.Column(db.Column(db.String(100), nullable = False))
+    email = db.Column(db.String(100), nullable = False)
+    password = db.Column(db.String(100), nullable = False)
     def __repr__(self):
         return f'<User {self.id}>'
 
