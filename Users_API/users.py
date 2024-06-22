60b48f62ce0a55185b81dedc96ca7363f54aed63
JoseFrancisc0 <83974552+JoseFrancisc0@users.noreply.github.com>
2024-06-21 19:26:00 -0500

    Update users.py

--- Users_API/users.py
+++ Users_API/users.py
@@ -18,7 +18,7 @@ class User(db.Model):
     nombre = db.Column(db.String(100), nullable = False)
     altura = db.Column(db.Numeric(4, 2), nullable = False)
     peso = db.Column(db.Numeric(4, 1), nullable = False)
-    foto = db.Column(db.String(200), nullable = False)
+    foto = db.Column(db.String(255), nullable = False)
     def __repr__(self):
         return f'<User {self.email}>'
 
